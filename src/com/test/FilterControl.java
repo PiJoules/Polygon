26f33b5f2c0794d11b338eef295f93e4cbdf4737
Leo is awesome <lc599@drexel.edu>
2014-07-30 18:18:26 -0400

    started working on filter select

--- src/com/test/FilterControl.java
+++ src/com/test/FilterControl.java
@@ -1,15 +1,39 @@
 package com.test;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 
 public class FilterControl extends Activity {
+    
+    private Button done;
 
     @Override
     public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+        super.onCreate(icicle); // call the parent class' onCreate method
         
+        // hide the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        // hide the notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        // lock the view to a vertical portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        
+        setContentView(R.layout.filter_control_layout);
+        
+        done = (Button) findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
     
 }

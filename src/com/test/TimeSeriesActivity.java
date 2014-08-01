9878092017b86372022cbdf3418b9f6f6e6bd2c8
Leo is awesome <lc599@drexel.edu>
2014-08-01 18:51:39 -0400

    commented the Time Series Plot

--- src/com/test/TimeSeriesActivity.java
+++ src/com/test/TimeSeriesActivity.java
@@ -1,5 +1,7 @@
 package com.test;
 
+// these are all the classes from other packages necessary for plotting the
+// accelerometer data
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
@@ -24,24 +26,35 @@ import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYStepMode;
 import java.util.ArrayList;
 
-
+// the TimeSeriesActivity is an Activity that uses the SensorEventListener for
+// getting accelerometer values
 public class TimeSeriesActivity extends Activity implements SensorEventListener{
 
-    private XYPlot plot;
-    private Button done, start_pause, reset, show_hide;
-    private TextView count;
+    private XYPlot plot; // the plot on which the graphs will be placed
+    private Button done, start_pause, reset, show_hide; // done, start, pause, reset, show, and hide buttons
+    private TextView count; // the view on the screen that displays the threshold counts for each vector
+    
+    // the initial arraylists holding the values for each vector
+    // and one for the constant threshold value
     private ArrayList<Number> XAccel, YAccel, ZAccel, threshold;
+    
+    // the series that will be plotted on the plot
     private SimpleXYSeries XAccelSeries, YAccelSeries, ZAccelSeries, thresholdSeries;
-    private final int ARRAYSIZE = 100;
-    private int sensorCount = 1;
-    private Accelerometer accel;
-    private boolean paused = false;
-    private final ArrayList<Number> xRange = new ArrayList<Number>();
+    private final int ARRAYSIZE = 100; // number of data points per series that will be displayed on the plot
+    private int sensorCount = 1; // the number of data points retrieved form accelerometer
+    private Accelerometer accel; // the accelerometer object from which we received accelerometer values
+    private boolean paused = false; // whether the plot is paused or not
+    private final ArrayList<Number> xRange = new ArrayList<Number>(); // range of x vals to plot against
+    
+    // formatters for each series that decorate the lines
+    // hide is the transparent decoration for hiding the threshold if necessary
     private LineAndPointFormatter XAccelFormat, YAccelFormat, ZAccelFormat, thresholdFormat, hide;
+    
+    // accelerometer file manager for handling the text file that stores alpha and sma values
     private AccelerometerFileManager afm;
-    private boolean showThreshold = true;
-    private int[] counts = new int[3];
-    private boolean[] highFlags = {false, false, false};
+    private boolean showThreshold = true; // whether or not the threshold graph is displayed
+    private int[] counts = new int[3]; // threshold count for each vector
+    private boolean[] highFlags = {false, false, false}; // flags for each vector to determine if they are above the threshold or not
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -56,26 +69,35 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
         // lock the view to a vertical portrait orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
         
+        // set the view the layout of the time_series_example.xml file
         setContentView(R.layout.time_series_example);
         
+        // initialize the acceleromater
         accel = new Accelerometer(this,this);
-        accel.setShouldFilter(false);
         
+        // initiliaze the accelerometer file manager
         afm = new AccelerometerFileManager(this, "");
         
+        // initialize the dynamic arraylists
         XAccel = new ArrayList<Number>();
         YAccel = new ArrayList<Number>();
         ZAccel = new ArrayList<Number>();
         threshold = new ArrayList<Number>();
         
+        // fill up the arraylists with initial values
         for (int i = 0; i < ARRAYSIZE; i++){
             XAccel.add(0);
             YAccel.add(0);
             ZAccel.add(0);
+            
+            // the fourth element in the accelerometer file indicates
+            // the threshold
             threshold.add(afm.getAccelData()[3]);
             xRange.add(sensorCount++);
         }
         
+        // initialize the done button
+        // once clicked, it will close the plot and return to the previous screen
         done = (Button) findViewById(R.id.done);
         done.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -83,6 +105,8 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
             }
         });
         
+        // initilaize the start_pause button
+        // once clicked, it either pauses the plot or starts it
         start_pause = (Button) findViewById(R.id.start_pause);
         start_pause.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -97,6 +121,8 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
             }
         });
         
+        // initilaize the reset button
+        // once clicked, it resets the threshold counts for each of the vectors
         reset = (Button) findViewById(R.id.reset);
         reset.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -106,6 +132,8 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
             }
         });
         
+        // initialize the show_hide button
+        // once clicked, it either shows or hides the threshild line on the plot
         show_hide = (Button) findViewById(R.id.show_hide);
         show_hide.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -120,67 +148,79 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
             }
         });
         
+        // initilaize the textview that shows the counts on the graph
         count = (TextView) findViewById(R.id.count);
         
+        // setup and decorate the plot
         plot = (XYPlot) findViewById(R.id.plot1);
-        plot.getGraphWidget().setSize(new SizeMetrics(0.9f, SizeLayoutType.RELATIVE, 1f, SizeLayoutType.RELATIVE));
-        plot.getGraphWidget().setAnchor(AnchorPosition.RIGHT_MIDDLE);
-        plot.setRangeUpperBoundary(11, BoundaryMode.FIXED);
-        plot.setRangeLowerBoundary(-11, BoundaryMode.FIXED);
-        plot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 1);
-        plot.getLegendWidget().setSize(new SizeMetrics(30, SizeLayoutType.ABSOLUTE, 300, SizeLayoutType.ABSOLUTE));
-        plot.getLegendWidget().position(50, XLayoutStyle.ABSOLUTE_FROM_RIGHT, 50, YLayoutStyle.ABSOLUTE_FROM_BOTTOM);
-        plot.getLegendWidget().setAnchor(AnchorPosition.RIGHT_BOTTOM);
-        plot.setRangeLabel("Acceleration (m/s^2)");
-        plot.setDomainLabel("Data Point Number");
+        plot.getGraphWidget().setSize(new SizeMetrics(0.9f, SizeLayoutType.RELATIVE, 1f, SizeLayoutType.RELATIVE)); // asjust size of plot
+        plot.getGraphWidget().setAnchor(AnchorPosition.RIGHT_MIDDLE); // center the plot vertically
+        plot.setRangeUpperBoundary(11, BoundaryMode.FIXED); // set the upper limit for the y axiz
+        plot.setRangeLowerBoundary(-11, BoundaryMode.FIXED); // set the lower limit for the y axis
+        plot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 1); // set the range between each tick on the y axis
+        plot.getLegendWidget().setSize(new SizeMetrics(30, SizeLayoutType.ABSOLUTE, 300, SizeLayoutType.ABSOLUTE)); // resize the legend
+        plot.getLegendWidget().position(50, XLayoutStyle.ABSOLUTE_FROM_RIGHT, 50, YLayoutStyle.ABSOLUTE_FROM_BOTTOM); // add margins to the legend
+        plot.getLegendWidget().setAnchor(AnchorPosition.RIGHT_BOTTOM); // align the legend to the bottom right corner of the screen
+        plot.setRangeLabel("Acceleration (m/s^2)"); // set the y axis label
+        plot.setDomainLabel("Data Point Number"); // set the x axis label
         
-        if (afm.getFilterType() == 0){
+        // check what type of filter should be used
+        if (afm.getFilterType() == 0){ // plot the raw data from the accelerometer if no filter
             plot.setTitle("Raw Accelerometer Data");
             accel.setShouldFilter(false);
         }
-        else if (afm.getFilterType() == 1){
+        else if (afm.getFilterType() == 1){ // plot the filtered data form the accelerometer using the alpha filter
             plot.setTitle("Alpha Filter Ouput with Alpha Value (" + afm.getAccelData()[1] + ")");
             accel.setShouldFilter(true);
             accel.setFilter(true);
             accel.setAlpha(afm.getAccelData()[1]);
         }
-        else if (afm.getFilterType() == 2){
+        else if (afm.getFilterType() == 2){ // plot the fltered data from the accelerometer using the sma filter
             plot.setTitle("SMA Filter Ouput with Period Count of (" + afm.getAccelData()[2] + ")");
             accel.setShouldFilter(true);
             accel.setFilter(false);
             accel.setPeriods((int) afm.getAccelData()[2]);
         }
         
-        // create XYSeries from xRange and 
+        // create XYSeries from xRange and the individual vectors and assign their titles
         XAccelSeries = new SimpleXYSeries(xRange, XAccel, "XAccel");
         YAccelSeries = new SimpleXYSeries(xRange, YAccel, "YAccel");
         ZAccelSeries = new SimpleXYSeries(xRange, ZAccel, "ZAccel");
         thresholdSeries = new SimpleXYSeries(xRange, threshold, "Threshold");
         
+        // color the XAccelSeries red
         XAccelFormat = new LineAndPointFormatter(
                 Color.RED,
                 Color.RED,
                 Color.TRANSPARENT,
                 null
         );
+        
+        // color the YAccelSeries green
         YAccelFormat = new LineAndPointFormatter(
                 Color.GREEN,
                 Color.GREEN,
                 Color.TRANSPARENT,
                 null
         );
+        
+        // color the ZAccelSeries blue
         ZAccelFormat = new LineAndPointFormatter(
                 Color.BLUE,
                 Color.BLUE,
                 Color.TRANSPARENT,
                 null
         );
+        
+        // color the thresholdSeries Cyan
         thresholdFormat = new LineAndPointFormatter(
                 Color.CYAN,
                 Color.CYAN,
                 Color.TRANSPARENT,
                 null
         );
+        
+        // make the threshold series thransparent if the show_hide button is pressed
         hide = new LineAndPointFormatter(
                 Color.TRANSPARENT,
                 Color.TRANSPARENT,
@@ -190,22 +230,32 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
         
     }
     
+    // method fro graphing the series on the xyplot
     private void graph(){
         plot.clear(); // clear the plot
 
-        // add a new series' to the xyplot:
+        // add a new series' to the xyplot using the appropriate formats
         plot.addSeries(XAccelSeries, XAccelFormat);
         plot.addSeries(YAccelSeries, YAccelFormat);
         plot.addSeries(ZAccelSeries, ZAccelFormat);
+        
+        // show the threshold series only if showThreshold is true
         if (showThreshold) plot.addSeries(thresholdSeries, thresholdFormat);
         else plot.addSeries(thresholdSeries, hide);
 
-        plot.redraw();
+        plot.redraw(); // redraw the new data
     }
 
+    // method called once the accelerometer values change
     public void onSensorChanged(SensorEvent event) {
-        accel.updateAccelVals(event); // update the accelerometer vals
+        accel.updateAccelVals(event); // update the accelerometer vals in the accel object
+        
+        // only perform the following if not paused
         if (!paused){
+            
+            // add the next filtered or unfiltered accelrometer readings
+            // to the end of each of the respective series and remove the 
+            // first values of each series to simulate a rolling plot
             XAccelSeries.addLast(sensorCount, accel.getAccel()[0]);
             YAccelSeries.addLast(sensorCount, accel.getAccel()[1]);
             ZAccelSeries.addLast(sensorCount, accel.getAccel()[2]);
@@ -215,6 +265,7 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
             ZAccelSeries.removeFirst();
             thresholdSeries.removeFirst();
             
+            // only check if each vector crossed the threshold if showthreshold is true
             if (showThreshold){
                 if (accel.getAccel()[0] > afm.getAccelData()[3] && !highFlags[0]){
                     counts[0]++;
@@ -239,10 +290,12 @@ public class TimeSeriesActivity extends Activity implements SensorEventListener{
                 }
             }
             
+            // displayt he values of each of the threshold counts
             count.setText("x:" + counts[0] + " y:" + counts[1] + " z:" + counts[2]);
             
+            // get another data point
             sensorCount++;
-            graph();
+            graph(); // graph the new data set
         }
     }
 
